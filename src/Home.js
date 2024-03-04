0bcbfb01a523b72f1fb38ce7ba4038c63e208521
AungPhyoLwin <aungphyolwin@gicjp.com>
2024-03-04 10:53:19 +0630

    home update

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MyanmarMonth from "./img/myanmarMonthPic.jpg";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { ImArrowRight } from "react-icons/im";
 export const Home = () => {
   return (
     <div className="container">
@@ -11,8 +12,13 @@ export const Home = () => {
       </div>
       <div className="row">
         <Link to="/months">
-          <div className="card img-thumbnail">
-            <img src={MyanmarMonth} alt="myanmar month zodic" />
+          <div>
+            <div className="card img-thumbnail img-fluid">
+              <img src={MyanmarMonth} alt="myanmar month zodic" />
+            </div>
+            <div className="d-flex align-items-end justify-content-center flex-column">
+              <ImArrowRight size={40} className="text-white"/>
+            </div>
           </div>
         </Link>
       </div>
